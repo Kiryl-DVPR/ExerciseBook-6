f16134c1f1bb824666c9d2d6b294bc6147c01560
Kiryl-DVPR <you@example.com>
2022-07-12 23:38:12 +0300

    exercise 6.13

--- 6.13.cpp
+++ 6.13.cpp
@@ -2,8 +2,29 @@
 #include <math.h>
 using namespace std;
 
+int calculateCharges(float);
+
 int main()
 {
+    float Hours; // Time
 
+    cout << "car\t" << "Hours\t" << "Charge" << endl;
 
+    for (int i = 1; i <= 3; i++)
+    {
+        cout << i <<"\t" ;
+        cin >> Hours;
+        cout << "\t" << calculateCharges(Hours) << endl;
+    }
+    
+
+}
+
+float calculateCharges(float Hours)
+{
+    if (Hours <= 3)
+    {
+        return 2;
+    }
+    
 }
