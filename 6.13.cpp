d67acbc3dad8b3d43734f8161503916817a41060
Kiryl <katana96@mail.ru>
2022-07-13 13:03:17 +0300

    Exercise 6.13

--- 6.13.cpp
+++ 6.13.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
-#include <math.h>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 
-int calculateCharges(float);
+double calculateCharges(double);
 
 int main()
 {
-    float Hours; // Time
+    double Hours1, Hours2, Hours3; // Time
 
-    cout << "car\t" << "Hours\t" << "Charge" << endl;
+    cout << "You need to enter parking time (less 24 hour): " << endl;
+    cin >> Hours1 >> Hours2 >> Hours3;
 
-    for (int i = 1; i <= 3; i++)
-    {
-        cout << i <<"\t" ;
-        cin >> Hours;
-        cout << "\t" << calculateCharges(Hours) << endl;
-    }
-    
+    cout << setw(8) << left << "Car" << setw(5) << right << "Hours" << setw(10) << "Charge" << endl;
+
+    cout << setw(8) << left << "1" << setw(5) << right << Hours1 << setw(9) << fixed << setprecision(2) << calculateCharges(Hours1) << endl; 
+    cout << setw(8) << left << "2" << setw(5) << right << fixed << setprecision(1) << Hours2 << setw(9) << fixed << setprecision(2) << calculateCharges(Hours2) << endl;
+    cout << setw(8) << left << "3" << setw(5) << right << fixed << setprecision(1) << Hours3 << setw(9) << fixed << setprecision(2) << calculateCharges(Hours3) << endl;  
 
 }
 
-float calculateCharges(float Hours)
-{
-    if (Hours <= 3)
+double calculateCharges(double Hours)
+{   
+    if (Hours <= 3 && Hours > 0)
     {
-        return 2;
-    }
-    
+        return 2.0;
+    };
+
+    if (Hours > 3 && Hours <= 16)
+    {
+        return (2 + ceil(Hours-3)*0.5);
+    };
+
+    if (Hours == 0)
+    {
+        return 0.0;
+    };
+
+    if (Hours == 24)
+    {
+        return 10;
+    };
+
 }
