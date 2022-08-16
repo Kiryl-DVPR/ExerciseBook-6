79580640e26bdecbc7a525a20c1bbc5ca856ef79
Kiryl <katana96@mail.ru>
2022-08-16 16:41:28 +0300

    Exercise 6.41

--- 6.41.cpp
+++ 6.41.cpp
@@ -1,18 +1,81 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
 int gcd( int x, int y);
+int gcdX(int x);
+int gcdY(int y);
 
 int main()
 {
-    
-   cout << gcd(9,8);
+    cout << gcd(27,27);
+
 }
+
+int i=1, r=1;
 
 int gcd( int x, int y)
 {
-    x = x % (x - 1);
-    gcd( x, y );
+    int xmax = gcdX(x);
+    int ymax = gcdY(y);
+
+    if( xmax > ymax )
+    {
+        return xmax;
+    }
+        return ymax;
+
+    if( xmax == ymax )
+    {
+        return xmax;
+    }
+        
+
+}
+
+int gcdX(int x)
+{
+    if (x==0)
+    {
+        return 0;
+    };
+
+    if (x==1)
+    {
+        return 1;
+    };
+    
+    int s = x % (x-r);
+ 
+    r++;
+ 
+    if(s==0)
+    {
+        int xmax = x-r+1;
+        return xmax;
+    }
+    gcdX(x);
+}
+
+int gcdY(int y)
+{
+    if (y==0)
+    {
+        return 0;
+    };
+
+    if (y==1)
+    {
+        return 1;
+    };
+    
+    int b = y % (y-i);
+ 
+    i++;
+ 
+    if(b==0)
+    {
+        int ymax = y-i+1;
+        return ymax;
+    }
+    gcdY(y);
 }
