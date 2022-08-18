a8af029ce859bc10b69e39a3117af9b706ba5091
Kiryl <katana96@mail.ru>
2022-08-18 12:02:04 +0300

    Test

---
+++ 6.48.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float y = 123.45678;
+    int x;
+    float b = static_cast<float>(x);
+    b=y;
+    
+    cout << b;
+}
