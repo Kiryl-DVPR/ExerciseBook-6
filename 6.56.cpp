7158520e5b2329a35a0c6c6954580a7300a5131e
Kiryl <katana96@mail.ru>
2022-08-25 17:08:11 +0300

    6.56 Finish

--- 6.56.cpp
+++ 6.56.cpp
@@ -5,25 +5,58 @@
 int multiply();
 
 int main()
-{
-    multiply();
+{    
+    int i = 1;
+
+    while ( i )
+    {
+        int Multiply = multiply();
+
+        int MyMultiply;
+        std :: cin >> MyMultiply;
+
+        if ( MyMultiply == Multiply && i != 3 )
+        {
+            std::cout << "Very well!!! " << std::endl;
+        }
+        else
+        {   
+            while ( MyMultiply != Multiply )
+            {   
+                if(++i > 3)
+                {
+                    break;
+                };
+              
+                std::cout << "Incorrect. Try again: ";
+                std::cin >> MyMultiply;  
+
+                if ( MyMultiply == Multiply && i != 3)
+                {
+                    std::cout << "Very well!!! " << std::endl;
+                };
+
+            }
+            
+        }
+
+        if (i > 2)
+        {
+            std::cout << "You are loose";
+            break;
+        }
+        
+    }
 }
 
 int multiply()
 {   
     srand( time(0));
 
-    int a = rand()%9+1; 
-    int b = rand()%9+1;
-    int c = a*b;
-    int d;
-
-    std::cout << "Multiply " << a << " by " << b << " = ";
-    std::cin >> d;
-    if( d == c )
-    {
-        std::cout << " Very well ";
-    }
-        std::cout << " Incorrect. Try again ";
+    int a = rand()%9 + 1; 
+    int b = rand()%9 + 1;
     
+    std::cout << "Multiply " << a << " by " << b << " = ";     
+
+    return a*b;    
 }
